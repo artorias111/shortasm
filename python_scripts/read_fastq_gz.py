c77ae1e2604cc32efd5128621b7c9472f04acfa1
Shriram <alekhshriram@gmail.com>
2025-07-22 09:13:30 -0500

    canonicalize kmers

--- python_scripts/read_fastq_gz.py
+++ python_scripts/read_fastq_gz.py
@@ -3,8 +3,20 @@
 import gzip
 from Bio import SeqIO
 
+k = 31  # Set your desired k-mer length here - might have to be an input later
+
+
+def reverse_complement(sequence):
+    complement = str.maketrans('ACGTacgt', 'TGCAtgca') # this is cool
+    return sequence.translate(complement)[::-1]
+
+
 fastq_file = '../tests/fastq_files/6_Swamp_S1_18S_2019_minq7.fastq.gz'
 with gzip.open(fastq_file, 'rt') as f:
     for record in SeqIO.parse(f, "fastq"):
-        print(">"+record.id)
-        print(record.seq)
+        seq = str(record.seq)
+        for i in range(len(seq) - k + 1):
+            kmer = seq[i:i+k]
+            revcomp = reverse_complement(kmer)
+            canonical = min(kmer, revcomp)
+            print(f"kmer: {kmer}\trevcomp: {revcomp}\tcanonical: {canonical}")
